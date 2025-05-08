64115c9b454cdc17b21d10a0b164641c26357250
violin11909 <sira2violin@gmail.com>
2025-05-08 22:54:32 +0700

    upgrade spi master

--- src/SPI_Master.v
+++ src/SPI_Master.v
@@ -1,31 +1,80 @@
 module SPI_Master (
     input wire clk,           // System clock
     input wire reset,         // Reset signal
-    input wire miso,          // Data from SD card
-    output reg mosi,          // Data to SD card
-    output reg sck,           // SPI clock
-    output reg cs,            // Chip select
+    input wire start,         // Trigger transfer เริ่มส่งข้อมูล
+    input wire [7:0] data_in, // byte to send
     output reg [7:0] data_out // Read data
+    output reg done,          // 1 when transfer is done
+    output reg busy,
+
+    output reg sck,           // slave's clock
+    output reg mosi,
+    input wire miso,
+    output reg cs             // chip selector
 );
 
-    reg [7:0] command;
-    reg [2:0] bit_counter;
-    
+    //parameters
+    parameter CLK_DIV = 125; //adjust this for sck speed
+
+    //internal registers
+    reg [7:0] shift_reg; //data register for sending/receiving
+    reg [2:0] bit_cnt; //count from 0 to 7 (8 bits)
+    reg [7:0] clk_div_cnt; //clock divider counter
+    reg sck_int; //internal clock phase (o or 1)
+    reg state; // 0 = idle, 1 = transfering
+
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            cs <= 1; mosi <= 1; sck <= 0;
-            bit_counter <= 0;
-            command <= 8'hFF; // Dummy data for SPI
+            shift_reg <= 8'h00;
+            data_out <= 8'h00;
+            bit_cnt <= 3'd0;
+            clk_div_cnt <= 8'd0;
+            sck <= 1'b0;
+            mosi <= 1'b1;
+            cs <= 1'b1;
+            busy <= 1'b0;
+            done <= 1'b0;
+            state <= 1'b0;
         end else begin
-            sck <= ~sck; // Toggle SPI clock
-            if (sck) begin
-                mosi <= command[7]; // Send command bit-by-bit
-                command <= {command[6:0], miso}; // Shift in data
-                bit_counter <= bit_counter + 1;
-                if (bit_counter == 7) begin
-                    data_out <= command; // Store received byte
+            done <= 1'b0;
+
+            //FSM
+            if (state == 0) begin //idle
+                if (start) begin
+                    cs <= 1'b0; //start using this slave
+                    busy <= 1'b1;
+                    state <= 1; //next state
+                    bit_cnt <= 3'd7;
+                    shift_reg <= data_in;
+                    mosi <= data_in[7];
+                    sck <= 1'b0;
+                    sck_int <= 1'b0;
+                    clk_div_cnt <= 0;
+                end
+            end else begin //transferring
+                if (clk_div_cnt == CLK_DIV) begin
+                    clk_div_cnt <= 0;
+                    sck_int <= ~sck_int;
+                    sck <= sck_int;
+
+                    if (sck_int == 1'b1) begin
+                        shift_reg <= {shift_reg[6:0], miso};
+                        if (bit_cnt == 0) begin
+                            state <= 0;
+                            busy <= 0;
+                            done <= 1;
+                            cs <= 1'b1;
+                            data_out <= {shift_reg[6:0], miso}; //final result
+                        end else begin
+                            bit_cnt <= bit_cnt - 1;
+                            mosi <= shift_reg[6]; //actually, it's just dummy
+                        end
+                    end
+                end else begin
+                    clk_div_cnt <= clk_div_cnt + 1;
                 end
             end
         end
     end
+
 endmodule
