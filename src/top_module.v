d20747119196fae5cf4bbbabeb6a868d2798b48e
violin11909 <sira2violin@gmail.com>
2025-05-09 11:51:06 +0700

    fix some wires in top_module(not finished yet)

--- src/top_module.v
+++ src/top_module.v
@@ -2,6 +2,7 @@ module top_module (
     input wire clk,
     input wire button,
     input wire miso, // Data จาก SD
+    input wire reset,
     output wire mosi, sck, cs, // SPI Signals
     output wire hsync, vsync,
     output wire [3:0] red, green, blue
@@ -14,28 +15,36 @@ module top_module (
 
     SPI_Master spi (
         .clk(clk),
-        .reset(0), 
-        .miso(miso), 
-        .mosi(mosi), 
-        .sck(sck), 
-        .cs(cs), 
+        .reset(reset), 
+        .miso(miso), //master in slave out
+        .mosi(mosi), //master out slave in
+        .sck(sck), //slave clk
+        .cs(cs), //chip select
         .data_out(sd_data)
     );
 
+    wire [16:0] read_addr; //vga to buffer
+    wire [15:0] pixel_data; //buffer to vga
+
     frame_buffer buffer (
         .clk(clk), 
-        .pixel_data(sd_data), 
-        .pixel_addr(pixel_addr), 
-        .vga_pixel(pixel_data)
+        .we(write_en), //from sd controller
+        .write_pixel(write_data),
+        .write_addr(write_addr),
+        .read_addr(read_addr), //finish
+        .vga_pixel(pixel_data) //finish
     );
 
     VGA_Controller vga (
         .clk(clk), 
-        .hsync(hsync), 
-        .vsync(vsync), 
-        .red(red), 
-        .green(green), 
-        .blue(blue)
+        .hsync(hsync), //finish
+        .vsync(vsync), //finish
+        .red(red), //finish
+        .green(green), //finish
+        .blue(blue), //finish
+        .pixel_data(pixel_data), //finish
+        .active_video(),
+        .read_addr(read_addr) //finish
     );
 
     button_switch btn (
